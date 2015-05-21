1bd81893fa6cbdfe5caa2fb1eef78992f7c628fc
Charles Lambert <c.lambert1213@gmail.com>
2015-05-21 15:17:52 -0700

    Version 6

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,14 +1,6 @@
-def lines(board)
-  a = board.each_slice(3).to_a
-  a += a.transpose
-  a += [board.values_at(0,4,8),board.values_at(2,4,6)]
-  return a
-end
-
 players = ["x","o"]
-board = Array.new(9," ")
 until not board.include?(" ") or (lines(board).find{|line| line[0] != " " and line.uniq.length == 1})
-  puts board.each_slice(3).to_a.map{|r|r.join(" | ")}.join("\n- * - * -\n") + "\n"
+  Board.draw
   print "Enter a valid move for #{players[0]}:  "
   move = gets.to_i
   while(board[move] != " ")
