21166ccc61d740be90250a4e3cf15fb3a5d963a3
Charles Lambert <c.lambert1213@gmail.com>
2015-05-21 14:35:16 -0700

    Version 3

--- Board.rb
+++ Board.rb
@@ -1,2 +1,14 @@
 class Board()
+	def initialize
+		@board = Array.new(9," ")
+	end
+	def lines
+ 		a = @board.each_slice(3).to_a
+ 		a += a.transpose
+ 		a += [@board.values_at(0,4,8),@board.values_at(2,4,6)]
+		return a
+	end
+	def draw
+		puts @board.each_slice(3).to_a.map{|r|r.join(" | ")}.join("\n- * - * -\n") + "\n"
+	end
 end
